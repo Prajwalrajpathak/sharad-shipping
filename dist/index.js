ef49d061380e91468b72a0a8f513d85dacd73636
prajwal <pathakprajwalraj@gmail.com>
2022-07-26 21:31:27 +0545

    deploy

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+function sendmessage(){
+
+   
+
+
+
+    var Name= document.getElementById("fullname");
+    var Email= document.getElementById("demail");
+    var Messege= document.getElementById("dmessege");
+    alert(Name);
+
+}
+
+function track() {
+
+   windows.open("/home/prp/Downloads/website-templates-master/unique-free-responsive-html5-template/track.html")
+    
+
+}
